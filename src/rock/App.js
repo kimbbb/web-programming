ef6f33f00f2858ee4b9cf3816a8af11b28c44e77
김민지 <wehouse201@naver.com>
2023-03-31 09:41:09 +0900

    1

--- src/rock/App.js
+++ src/rock/App.js
@@ -4,19 +4,26 @@ import HandIcon from './HandIcon';
 import { compareHand, generateRandomHand } from './utils';
 import {useState} from 'react';
 
-let win=0
-let lose=0
+
+let win=[]
+let lose=[]
+let non=[]
 function getResult(me, other) {
   const comparison = compareHand(me, other);
   if (comparison > 0) {
+    win.push('승리')
+    console.log(win)
     return '승리';
   }
   if (comparison < 0) {
+    lose.push('패배')
+    console.log(lose)
     return '패배';
   }
+  non.push('무승부')
+  console.log(non)
   return '무승부';
 }
-
 
 
 function App() {
@@ -38,7 +45,7 @@ function App() {
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)} {}승 {}무 {}패</p>
+      <p>{getResult(hand, otherHand)} {win.length/2}승 {non.length/2}무 {lose.length/2}패</p>
       <div>
         <HandIcon value={hand} />
         VS
